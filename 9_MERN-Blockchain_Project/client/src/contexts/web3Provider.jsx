3e60a1dafc9d26599b82bcee8b004ceca1784309
nirbhay2001 <nirbhayggupta2001@gmail.com>
2024-10-20 18:06:10 +0530

    pushh all blockchain project

---
+++ 9_MERN-Blockchain_Project/client/src/contexts/web3Provider.jsx
@@ -0,0 +1,19 @@
+import { Web3Context } from "./createWeb3Context";
+
+const Web3Provider = () => {
+
+        const [web3State, setWeb3State] = useState({
+            contractInstance:null,
+            selectedAccount:null,
+        })
+
+    return ( 
+        <>
+
+        </>
+     );
+}
+
+
+ 
+export default Web3Provider;
